36911a039638e3e005691f500c6adf829bb96fa3
SeokGoo Lee <visualtuner@gmail.com>
2024-09-13 13:09:43 +0900

    머터리얼 아이콘 삭제

--- src/components/GlobalNav.vue
+++ src/components/GlobalNav.vue
@@ -1,36 +1,18 @@
 <script setup>
-    import { GSymbol } from 'vue-material-symbols';
+    
 </script>
 <template>
     <nav id="gnb">
         <RouterLink to="/">
-            <GSymbol
-                icon="home"
-                type="outlined"
-                :weight="300"
-                :grade="0"
-                :size="28"
-            />
+            
             <p>홈</p>
         </RouterLink>
         <RouterLink to="/about">
-            <GSymbol
-                icon="variable_insert"
-                type="outlined"
-                :weight="300"
-                :grade="0"
-                :size="28"
-            />
+            
             <p>인풋</p>
         </RouterLink>
         <RouterLink to="/">
-            <GSymbol
-                icon="buttons_alt"
-                type="outlined"
-                :weight="300"
-                :grade="0"
-                :size="28"
-            />
+            
             <p>버튼</p>
         </RouterLink>
     </nav>
@@ -49,7 +31,7 @@
         left: 0;
         margin: 0;
         z-index: 1000;
-        box-shadow: 0 0 0 1px rgba(0,0,0,0.1);
+        box-shadow: 0 0 0 1px rgba(0,0,0,0.06);
     }
     #gnb a {
         flex: 1 0 0;
@@ -63,10 +45,6 @@
         justify-content: center;
         text-align: center;
         -webkit-tap-highlight-color: transparent;
-    }
-
-    #gnb a .g-symbol {
-
     }
 
     #gnb a p {
