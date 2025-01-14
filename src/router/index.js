ac60d76ae9e087d38f416c1046cb427473402528
SeokGoo Lee <visualtuner@gmail.com>
2025-01-14 11:12:30 +0900

    히스토리 모드 원복

--- src/router/index.js
+++ src/router/index.js
@@ -10,8 +10,8 @@ function loadView (view) {
 }
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-    //history: createWebHashHistory(),
+	//history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
 	routes: [
 		{
 			path: "/",
